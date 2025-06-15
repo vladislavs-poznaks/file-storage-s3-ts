1a300f27996a148571e1c786911d5b5c00a3bee3
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2025-06-15 20:17:27 +0300

    added thumbnail upload functionality

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,12 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, UserForbiddenError } from "./errors";
+import path from "path";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+const MAX_UPLOAD_SIZE = 10 << 20
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -47,7 +19,37 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData()
 
-  return respondWithJSON(200, null);
+  const file = formData.get("thumbnail")
+
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing")
+  }
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File too large")
+  }
+
+  if (!['image/jpeg', 'image/png'].includes(file.type)) {
+    throw new BadRequestError("File type not supported")
+  }
+
+  const [_, extension] = file.type.split('/')
+
+  const filePath = path.join(cfg.assetsRoot, `${videoId}.${extension}`)
+
+  Bun.write(filePath, file)
+
+  const url = `http://localhost:8091/${filePath}`
+  
+  const metadata = getVideo(cfg.db, videoId)
+
+  if (metadata?.userID !== userID) {
+    throw new UserForbiddenError("Forbidden")
+  }
+
+  updateVideo(cfg.db, {...metadata, thumbnailURL: url})
+
+  return respondWithJSON(200, metadata);
 }
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import {
   handlerVideosRetrieve,
 } from "./api/video-meta";
 import { handlerUploadVideo } from "./api/videos";
-import { handlerUploadThumbnail, handlerGetThumbnail } from "./api/thumbnails";
+import { handlerUploadThumbnail } from "./api/thumbnails";
 import { handlerReset } from "./api/reset";
 import { ensureAssetsDir } from "./api/assets";
 import spa from "./app/index.html";
@@ -48,9 +48,9 @@ Bun.serve({
     "/api/thumbnail_upload/:videoId": {
       POST: withConfig(cfg, handlerUploadThumbnail),
     },
-    "/api/thumbnails/:videoId": {
-      GET: withConfig(cfg, handlerGetThumbnail),
-    },
+    // "/api/thumbnails/:videoId": {
+    //   GET: withConfig(cfg, handlerGetThumbnail),
+    // },
     "/api/video_upload/:videoId": {
       POST: withConfig(cfg, handlerUploadVideo),
     },
