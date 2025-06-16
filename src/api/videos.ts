77705044c9bc3255c31b4b66edce5400405b49de
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2025-06-16 19:26:39 +0300

    added video upload to s3

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,68 @@
 import { respondWithJSON } from "./json";
 
 import { type ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { S3Client, type BunRequest } from "bun";
+import { BadRequestError, UserForbiddenError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
+import path from "path";
+import crypto from "crypto";
+
+const MAX_UPLOAD_SIZE = 1 << 30
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
-  return respondWithJSON(200, null);
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  console.log("uploading video for video", videoId, "by user", userID);
+
+  const metadata = getVideo(cfg.db, videoId)
+
+  if (metadata?.userID !== userID) {
+    throw new UserForbiddenError("Forbidden")
+  }
+
+  const formData = await req.formData()
+
+  const file = formData.get("video")
+
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Video file missing")
+  }
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File too large")
+  }
+
+  if (!['video/mp4'].includes(file.type)) {
+    throw new BadRequestError("File type not supported")
+  }
+
+  const [_, extension] = file.type.split('/')
+
+  const tempPath = path.join(cfg.tempRoot, `${videoId}.${extension}`)
+
+  await Bun.write(tempPath, file)
+
+  const key = crypto.randomBytes(32).toString('hex')
+
+  const filename = `${key}.${extension}`
+
+  console.log('key', filename)
+
+  const s3file = S3Client.file(filename)
+  s3file.write(Bun.file(tempPath))
+
+  const url = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${filename}`
+
+  updateVideo(cfg.db, {...metadata, videoURL: url})
+
+  await Bun.file(tempPath).delete()
+
+  return respondWithJSON(200, metadata);
 }
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 import { newDatabase } from "./db/db";
 import type { Database } from "bun:sqlite";
+import { s3 } from "bun";
+import type { S3Client } from "bun";
 
 export type ApiConfig = {
   db: Database;
@@ -7,10 +9,12 @@ export type ApiConfig = {
   platform: string;
   filepathRoot: string;
   assetsRoot: string;
+  tempRoot: string;
   s3Bucket: string;
   s3Region: string;
   s3CfDistribution: string;
   port: string;
+  s3Client: S3Client;
 };
 
 const pathToDB = envOrThrow("DB_PATH");
@@ -18,6 +22,7 @@ const jwtSecret = envOrThrow("JWT_SECRET");
 const platform = envOrThrow("PLATFORM");
 const filepathRoot = envOrThrow("FILEPATH_ROOT");
 const assetsRoot = envOrThrow("ASSETS_ROOT");
+const tempRoot = envOrThrow("TEMP_ROOT");
 const s3Bucket = envOrThrow("S3_BUCKET");
 const s3Region = envOrThrow("S3_REGION");
 const s3CfDistribution = envOrThrow("S3_CF_DISTRO");
@@ -31,10 +36,12 @@ export const cfg: ApiConfig = {
   platform: platform,
   filepathRoot: filepathRoot,
   assetsRoot: assetsRoot,
+  tempRoot: tempRoot,
   s3Bucket: s3Bucket,
   s3Region: s3Region,
   s3CfDistribution: s3CfDistribution,
   port: port,
+  s3Client: s3,
 };
 
 function envOrThrow(key: string) {
