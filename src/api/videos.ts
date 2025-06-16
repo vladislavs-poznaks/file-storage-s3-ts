f1459fbb9a5daaa3e9c67228fd4f0b3d658bd8b0
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2025-06-16 21:57:58 +0300

    spawn command optimizations

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -8,6 +8,8 @@ import { getVideo, updateVideo } from "../db/videos";
 import path from "path";
 import crypto from "crypto";
 
+type aspectRatio = "portrait" | "landscape" | "other"
+
 const MAX_UPLOAD_SIZE = 1 << 30
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
@@ -49,20 +51,99 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
 
   await Bun.write(tempPath, file)
 
+  const aspectRatio = await getVideoAspectRatio(tempPath)
+
+  const processedTempPath = await processVideoForFastStart(tempPath)
+
+  await Bun.file(tempPath).delete()
+
   const key = crypto.randomBytes(32).toString('hex')
 
-  const filename = `${key}.${extension}`
-
-  console.log('key', filename)
+  const filename = `${aspectRatio}/${key}.${extension}`
 
   const s3file = S3Client.file(filename)
-  s3file.write(Bun.file(tempPath))
+  s3file.write(Bun.file(processedTempPath))
 
   const url = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${filename}`
 
   updateVideo(cfg.db, {...metadata, videoURL: url})
 
-  await Bun.file(tempPath).delete()
+  await Bun.file(processedTempPath).delete()
 
   return respondWithJSON(200, metadata);
+}
+
+const getVideoAspectRatio = async (filePath: string): Promise<aspectRatio> => {
+  const process = Bun.spawn([
+    'ffprobe',
+    '-v',
+    'error',
+    '-select_streams',
+    'v:0',
+    '-show_entries',
+    'stream=width,height',
+    '-of',
+    'json',
+    filePath,
+  ])
+
+  const outputText = await new Response(process.stdout).text();
+  const errorText = await new Response(process.stderr).text();
+
+  const exitCode = await process.exited;
+
+  if (exitCode !== 0) {
+    throw new Error(`ffprobe error: ${errorText}`);
+  }
+
+  const output = JSON.parse(outputText)
+
+  if (!output.streams || output.streams.length === 0) {
+    throw new Error("No video streams found");
+  }
+
+  const {height, width} = output.streams[0]
+
+  const aspectRatio = width / height;
+  const landscapeRatio = 16 / 9;
+  const portraitRatio = 9 / 16;
+  const epsilon = 0.01;
+
+  if (Math.abs(aspectRatio - landscapeRatio) < epsilon) {
+      return "landscape";
+  } 
+  
+  if (Math.abs(aspectRatio - portraitRatio) < epsilon) {
+      return "portrait";
+  }
+
+  return "other"
+}
+
+const processVideoForFastStart = async (inputFilePath: string): Promise<string> => {
+  const outputFilePath = `${inputFilePath}.processed`
+
+  const process = Bun.spawn([
+    'ffmpeg',
+    '-i',
+    inputFilePath,
+    '-movflags',
+    'faststart',
+    '-map_metadata',
+    '0',
+    '-codec',
+    'copy',
+    '-f',
+    'mp4',
+    outputFilePath,
+  ])
+
+  const errorText = await new Response(process.stderr).text();
+  const exitCode = await process.exited;
+
+  if (exitCode !== 0) {
+    throw new Error(`FFmpeg error: ${errorText}`);
+  }
+
+  return outputFilePath
 }
